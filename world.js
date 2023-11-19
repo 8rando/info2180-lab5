b82e0a5c7cd60d381a5f93a7d7700e128b775baf
8rando <brandonlabrad@gmail.com>
2023-11-19 11:38:12 -0500

    Edited Files: index.html, world.php, world.js, styles.css

--- world.js
+++ world.js
@@ -1,21 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('lookup').addEventListener('click', function(e) {
+    document.getElementById('lookup_country').addEventListener('click', function(e) {
         e.preventDefault();
-
-        let country = document.getElementById('country').value;
-
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET', 'world.php?country=' + encodeURIComponent(country), true);
-        xhr.onload = function() {
-            if (xhr.status == 200) {
-                document.getElementById('result').innerHTML = xhr.responseText;
-            } else {
-                console.error(xhr.statusText);
-            }
-        };
-        xhr.onerror = function() {
-            console.error(xhr.statusText);
-        };
-        xhr.send();
+        makeRequest('country');
+    });
+    
+    document.getElementById('lookup_cities').addEventListener('click', function(e) {
+        e.preventDefault();
+        makeRequest('cities');
     });
 });
+
+function makeRequest(type) {
+    let country = document.getElementById('country').value;
+    let query = 'country=' + encodeURIComponent(country);
+    if (type === 'cities') {
+        query += '&lookup=cities';
+    }
+    
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'world.php?' + query, true);
+    xhr.onload = function() {
+        if (xhr.status == 200) {
+            document.getElementById('result').innerHTML = xhr.responseText;
+        } else {
+            console.error(xhr.statusText);
+        }
+    };
+    xhr.onerror = function() {
+        console.error(xhr.statusText);
+    };
+    xhr.send();
+}
--- world.php
+++ world.php
@@ -8,25 +8,50 @@ $unix_socket = '/Applications/MAMP/tmp/mysql/mysql.sock'; // Replace with the co
 
 $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4;unix_socket=$unix_socket", $username, $password);
 
+header('Content-Type: text/html; charset=utf-8');
 
-// Check if 'country' is passed as a GET request variable
-if (isset($_GET['country']) && $_GET['country'] != '') {
+// Check if the 'lookup' parameter is set to 'cities' and 'country' is provided
+if (isset($_GET['lookup']) && $_GET['lookup'] === 'cities' && isset($_GET['country'])) {
+    $country = $_GET['country'];
+    $stmt = $conn->prepare("SELECT cities.name, cities.district, cities.population 
+                            FROM cities 
+                            JOIN countries ON cities.country_code = countries.code 
+                            WHERE countries.name LIKE ?");
+    $stmt->execute(["%$country%"]);
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // Output cities in an HTML table
+    echo '<table>';
+    echo '<tr><th>Name</th><th>District</th><th>Population</th></tr>';
+    foreach ($results as $row) {
+        echo '<tr>';
+        echo '<td>' . htmlspecialchars($row['name'] ?? 'N/A') . '</td>';
+        echo '<td>' . htmlspecialchars($row['district'] ?? 'N/A') . '</td>';
+        echo '<td>' . htmlspecialchars($row['population'] ?? 'N/A') . '</td>';
+        echo '</tr>';
+    }
+    echo '</table>';
+} else if (isset($_GET['country'])) {
+    // This is the country lookup logic
     $country = $_GET['country'];
     $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE ?");
     $stmt->execute(["%$country%"]);
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // Output countries in an HTML table
+    echo '<table>';
+    echo '<tr><th>Name</th><th>Continent</th><th>Independence Year</th><th>Head of State</th></tr>';
+    foreach ($results as $row) {
+        echo '<tr>';
+        echo '<td>' . htmlspecialchars($row['name'] ?? 'N/A') . '</td>';
+        echo '<td>' . htmlspecialchars($row['continent'] ?? 'N/A') . '</td>';
+        echo '<td>' . htmlspecialchars($row['independence_year'] ?? 'N/A') . '</td>';
+        echo '<td>' . htmlspecialchars($row['head_of_state'] ?? 'N/A') . '</td>';
+        echo '</tr>';
+    }
+    echo '</table>';
 } else {
-    // If no country is specified, select all countries
-    $stmt = $conn->query("SELECT * FROM countries");
+    // If no country is specified, you might want to handle this case as well
+    echo 'Please enter a country name.';
 }
-
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-header('Content-Type: text/html; charset=utf-8');
-
 ?>
-
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= htmlspecialchars($row['name'] ?? '') . ' is ruled by ' . htmlspecialchars($row['head_of_state'] ?? ''); ?></li>
-<?php endforeach; ?>
-</ul>
